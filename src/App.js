0368b890ecb6d100e4fea38c1786056306dccb0f
MiguelFidel <kazuki.accel11@gmail.com>
2023-05-07 05:31:06 +0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Home from './components/home';
 import Footer from './components/global-components/footer';
 import Header from './components/global-components/header';
+import { BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header/>
       <Home/>
       <Footer/>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // BASE URL
 export const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_BASE_URL,
   timeout: 1000,
 });
 
