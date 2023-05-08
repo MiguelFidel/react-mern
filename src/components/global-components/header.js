a14c866a0c90bf184ab2d7c5922ebfc3bc0c4c24
MiguelFidel <kazuki.accel11@gmail.com>
2023-05-08 12:35:02 +0800

    fix

--- src/components/global-components/header.js
+++ src/components/global-components/header.js
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <div className="bg-light text-center text-lg-start">
             <div className="text-center p-3 bold">
-                <h1>Simple MERN App</h1>
+                <h1>Mern Crud App</h1>
             </div>
         </div>
     );
